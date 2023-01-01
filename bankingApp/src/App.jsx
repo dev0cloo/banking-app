dd687126bc87196ef10a33fead4a541d50248d5f
dev0cloo <etor.ocloo@gmail.com>
2023-01-01 20:55:14 +0000

    Add components and Edit style.js

--- bankingApp/src/App.jsx
+++ bankingApp/src/App.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
 
-const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+import styles from "./style";
+// imports components from index.js to save code repetition
+import {
+  Navbar,
+  Hero,
+  Billing,
+  CardDeal,
+  Business,
+  Clients,
+  CTA,
+  Stats,
+  Footer,
+  Testimonials,
+} from "./components";
 
-export default App
+const App = () => (
+  <div className="bg-primary w-full overflow-hidden">
+    {/* navbar classnames represent imported styles from ./style*/}
+    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Navbar /></div>
+    </div>
+
+    <div className={`bg-primary ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}><Hero /></div>
+    </div>
+
+    <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Stats />
+        <Business />
+        <Billing />
+        <CTA />
+        <CardDeal />
+        <Clients />
+        <Testimonials />
+        <Footer />
+      </div>
+    </div>
+  </div>
+);
+
+export default App;
