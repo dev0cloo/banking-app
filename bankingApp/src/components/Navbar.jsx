fdb525b5e8c5ebded15d6f249d469450d76c3d78
dev0cloo <etor.ocloo@gmail.com>
2023-01-01 23:06:04 +0000

    Update Navbar.jsx

--- bankingApp/src/components/Navbar.jsx
+++ bankingApp/src/components/Navbar.jsx
@@ -3,21 +3,49 @@ import { menu, logo, close } from "../assets";
 import { navLinks } from "../constants";
 
 const Navbar = () => {
+  const [toggle, setToggle] = useState(false);
   return (
-    // classnames are from tailwindcss 
+    // classnames are from tailwindcss
     <nav className="w-full flex py-6 justify-between items-center navbar">
       <img src={logo} alt="hoobank" className="w-[124px] h-[32px]" />
+      {/* sets a navbar for desktops  */}
       <ul className="list-none sm:flex hidden justify-end items-center flex-1 ">
-        {/*  */}
         {navLinks.map((nav, index) => (
-          <li key={nav.id}
-          className={`font-poppins font-normal cursor-pointer text-[16px] text-white ${index === navLinks.length-1 ? 'mr-0' : 'mr-10'} `} >
-            <a href={`#${nav.id}`}>
-              {nav.title}
-            </a>
+          <li
+            key={nav.id}
+            className={`font-poppins font-normal cursor-pointer text-[16px] text-white ${
+              index === navLinks.length - 1 ? "mr-0" : "mr-10"
+            } `}
+          >
+            <a href={`#${nav.id}`}>{nav.title}</a>
           </li>
         ))}
       </ul>
+      {/* sets a navbar for mobile devices */}
+      <div className={`flex sm:hidden flex-1 justify-end items-center`}>
+        <img
+          src={toggle ? close : menu}
+          alt="menu"
+          className="w-[28px] h-[28px] object-contain"
+          onClick={() => setToggle(prev => !prev)}
+        />
+        <div
+          className={`${
+            toggle ? "flex" : "hidden"
+          } p-6 bg-black-gradient absolute rounded-xl sidebar top-20 right-0 mx-4 my-2 min-w-[140px]`}
+        >
+          <ul className="list-none flex flex-col justify-end items-center flex-1 gap-10 ">
+            {navLinks.map((nav, index) => (
+              <li
+                key={nav.id}
+                className={`font-poppins font-normal cursor-pointer text-[16px] text-white `}
+              >
+                <a href={`#${nav.id}`}>{nav.title}</a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </nav>
   );
 };
