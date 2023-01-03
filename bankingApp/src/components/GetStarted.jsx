a434dc671e336c2685143413be268a23eb84c8f6
dev0cloo <etor.ocloo@gmail.com>
2023-01-03 17:31:41 +0000

    Update GetStarted.jsx and Hero.jsx

--- bankingApp/src/components/GetStarted.jsx
+++ bankingApp/src/components/GetStarted.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import styles from "../style";
+import { arrowUp } from "../assets";
 
-const GetStarted = () => {
-  return (
-    <div>GetStarted</div>
-  )
-}
+const GetStarted = () => (
+   <div className={`${styles.flexCenter} w-[140px] h-[140px] rounded-full bg-blue-gradient p-1 cursor-pointer`} >
+    <div className={`${styles.flexCenter} bg-primary flex-col w-[100%] h-[100%] rounded-full`}>
+      <div className={`${styles.flexStart} flex-row gap-1`}>
+        <p className="font-poppins font-medium text-[18px] leading-[23px]">
+          <span className="text-gradient">Get</span>
 
-export default GetStarted
+        </p>
+        <img src={arrowUp} alt='arrow' className="w-[23px] h-[23px] object-contain"/>
+      </div>
+      <p className="font-poppins font-medium text-[18px] leading-[23px]">
+          <span className="text-gradient">Started</span>
+      </p>
+    </div>
+   </div>
+)
+
+export default GetStarted;
