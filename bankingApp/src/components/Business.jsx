2d5295406c2f0b0c0f748d40217f56dd6d685b15
dev0cloo <etor.ocloo@gmail.com>
2023-01-04 09:30:00 +0000

    Update Business.jsx

--- bankingApp/src/components/Business.jsx
+++ bankingApp/src/components/Business.jsx
@@ -1,9 +1,37 @@
 import { features } from "../constants";
 import Button from "./Button";
 import styles, { layout } from "../style";
+
+{
+  /* create FeatureCard component to render feature mappings */
+}
+const FeatureCard = ({ icon, title, content }) => (
+  <div className={`flex flex-row p-6 rounded-[20px] gap-6 feature-card`}>
+    <div
+      className={`w-[64px] h-[64px] rounded-full bg-dimBlue ${styles.flexCenter}`}
+    >
+      <img src={icon} alt="icon" className={`w-[50%] h-[50%] object-contain`} />
+    </div>
+    <div className={`flex-1 flex flex-col ml-3`}>
+      <h4
+        className={`font-poppins text-white text-[18px] font-semibold leading-[23px] mb-1`}
+      >
+        {title}
+      </h4>
+
+      <p
+        className={`${styles.paragraph}font-poppins text-dimWhite text-[16px] font-normal leading-[24px] mb-1`}
+      >
+        {content}
+      </p>
+    </div>
+  </div>
+);
+
 const Business = () => {
   return (
-    <section id="features" className={`layout.section`}>
+    <section id="features" className={`${layout.section}`}>
+      {/* controls the left side of the business component */}
       <div className={layout.sectionInfo}>
         <h2 className={`${styles.heading2} mt-5`}>
           You do the business <br className="sm:block hidden" /> We'll handle
@@ -15,6 +43,13 @@ const Business = () => {
           of credit cards on the market.
         </p>
         <Button styles="mt-10" />
+      </div>
+      {/* controls the right side of the business component */}
+      <div className={`${layout.sectionImg} flex-col`}>
+        {/* maps each feature  */}
+        {features.map((feature, index) => (
+          <FeatureCard key={feature.id} {...feature} index={index} />
+        ))}
       </div>
     </section>
   );
