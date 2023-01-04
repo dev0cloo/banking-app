74fa43f1de2e58de54ef03e76e4e1dfe96e06043
dev0cloo <etor.ocloo@gmail.com>
2023-01-04 15:08:24 +0000

    Update Testimonials.jsx

--- bankingApp/src/components/Testimonials.jsx
+++ bankingApp/src/components/Testimonials.jsx
@@ -1,39 +1,33 @@
 import React from "react";
 import { feedback } from "../constants";
 import styles, { layout } from "../style";
-import { quotes } from "../assets";
+import FeedbackCard from "./FeedbackCard";
 
-const FeatureCard = ({ content, name, title, img }) => (
-  <div className={`flex ${styles.flexStart} flex-col gap-10 feature-card py-12 px-12 rounded-[9px]`}>
-      <img src={quotes} alt="quote" className={``} />
 
-    <div className="flex">
-      <p className={`${styles.paragraph}`}>{content} </p>
-    </div>
-    <div className={`flex flex-row gap-4 items-center`}>
-      <img src={img} alt="person"/>
-      <div className={`flex flex-col`}>
-        <h6 className={``}>{name}</h6>
-        <p>{title}</p>
-      </div>
-    </div>
-  </div>
-);
 const Testimonials = () => (
-  <section className={`${layout.section} md:flex-col`}>
-    <div className={`flex flex-row justify-between items-center`}>
+  <section
+    id="clients"
+    className={`${styles.paddingY} ${styles.flexCenter} relative flex-col`}
+  >
+    <div className={`absolute z-[0] w-[60%] -right-[50%] h-[50%] rounded-full blue__gradient`} />
+
+    <div
+      className={`w-full flex sm:flex-row flex-col sm:mb-16 justify-between items-center relative z-[1] mb-6`}
+    >
       <h2 className={styles.heading2}>
         What people are
         <br className="sm:block hidden" /> saying about us
       </h2>
-      <p className={styles.paragraph}>
-        Everything you need to accept card payments and grow your business
-        anywhere on the planet.
-      </p>
+      <div className="w-full md:mt-0 mt-6">
+        <p className={`${styles.paragraph} text-left max-w-[450px]`}>
+          Everything you need to accept card payments and grow your business
+          anywhere on the planet.
+        </p>
+      </div>
     </div>
-    <div className={`flex flex-row gap-20  mt-20`}>
-      {feedback.map((feed) => (
-        <FeatureCard key={feed.id} {...feed} />
+    <div className={`flex flex-row flex-wrap justify-center sm:justify-start w-full feedback-container relative z-[1]  mt-20`}>
+      {feedback.map((card) => (
+        <FeedbackCard key={card.id} {...card} />
       ))}
     </div>
   </section>
