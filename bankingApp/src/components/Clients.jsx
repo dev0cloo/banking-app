7a17e72bdb17eb1ddac98e74027b8b737743c86d
dev0cloo <etor.ocloo@gmail.com>
2023-01-04 15:39:21 +0000

    Update Clients.jsx

--- bankingApp/src/components/Clients.jsx
+++ bankingApp/src/components/Clients.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import { clients } from "../constants";
+import styles from "../style";
 
-const Clients = () => {
-  return (
-    <div>Clients</div>
-  )
-}
+const Clients = () => (
+  <section id="clients" className={`${styles.flexCenter} my-4`}>
+    <div
+      className={`${styles.flexCenter}  w-full flex-wrap`}
+    >
+      {clients.map((client) => (
+        <div key={client.id} className={`${styles.flexCenter} sm:min-w-[192px] min-w-[120px] flex-1`}>
+          <img
+            src={client.logo}
+            alt={client.id}
+            className={`sm:w-[192px] w-[100px] object-contain`}
+          />
+        </div>
+      ))}
+    </div>
+  </section>
+);
 
-export default Clients
+export default Clients;
