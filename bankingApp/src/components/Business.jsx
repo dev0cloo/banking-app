61a2b16446e28b767b6d9a26f176b9e116a2cb1b
dev0cloo <etor.ocloo@gmail.com>
2023-01-05 04:23:01 +0000

    Update CardDeal with animations

--- bankingApp/src/components/Business.jsx
+++ bankingApp/src/components/Business.jsx
@@ -1,14 +1,19 @@
 import { features } from "../constants";
 import Button from "./Button";
 import styles, { layout } from "../style";
-import { normalText, card } from "./Variants";
+import { normalText, card, button } from "./Variants";
 import { motion } from "framer-motion";
 
 {
   /* create FeatureCard component to render feature mappings */
 }
 const FeatureCard = ({ icon, title, content }) => (
-  <motion.div variants={card} initial="hidden" whileInView="visible" className={`flex flex-row p-6 rounded-[20px] gap-6 feature-card`}>
+  <motion.div
+    variants={card}
+    initial="hidden"
+    whileInView="visible"
+    className={`flex flex-row p-6 rounded-[20px] gap-6 feature-card`}
+  >
     <div
       className={`w-[64px] h-[64px] rounded-full bg-dimBlue ${styles.flexCenter}`}
     >
@@ -35,16 +40,42 @@ const Business = () => {
     <section id="features" className={`${layout.section}`}>
       {/* controls the left side of the business component */}
       <div className={layout.sectionInfo}>
-        <motion.h2 variants={normalText} initial="hidden" whileInView="header" className={`${styles.heading2} mt-5`}>
+        <motion.h2
+          variants={normalText}
+          initial="hidden"
+          whileInView="header"
+          className={`${styles.heading2} mt-5`}
+        >
           You do the business, <br className="sm:block hidden" /> We'll handle
           the money.
         </motion.h2>
-        <motion.p variants={normalText} initial="hidden" whileInView="header"  className={`${styles.paragraph} max-w-[470px] mt-5`}>
+        <motion.p
+          variants={normalText}
+          initial="hidden"
+          whileInView="header"
+          className={`${styles.paragraph} max-w-[470px] mt-5`}
+        >
           With the right credit card, you can improve your financial life by
           building credit, earning rewards and saving money. But with hundreds
           of credit cards on the market.
         </motion.p>
-        <Button styles="mt-10" />
+        <motion.div
+          initial="hidden"
+          whileInView={{
+            opacity: [0, 0.5, 1],
+            y: [0, 15, 0],
+            transition: {
+              duration: 1,
+              times: [0, 0.5, 1],
+              delay: 1,
+            },
+          }}
+          whileHover="hov"
+          variants={button}
+          whileTap="click"
+        >
+          <Button styles="mt-10" />
+        </motion.div>
       </div>
       {/* controls the right side of the business component */}
       <div className={`${layout.sectionImg} flex-col justify-center`}>
--- bankingApp/src/components/CardDeal.jsx
+++ bankingApp/src/components/CardDeal.jsx
@@ -2,22 +2,59 @@ import React from "react";
 import { card } from "../assets";
 import styles, { layout } from "../style";
 import Button from "./Button";
+import { normalText, button } from "./Variants";
+import { motion } from "framer-motion";
 
 const CardDeal = () => (
   <section className={`${layout.section}`}>
     <div className={`${layout.sectionInfo}`}>
-      <h2 className={`${styles.heading2} mt-5`}>
-        Find a better card deal <br className="sm:block hidden"/> in few
-        easy steps.
-      </h2>
-      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+      <motion.h2
+        variants={normalText}
+        initial="hidden"
+        whileInView="header"
+        className={`${styles.heading2} mt-5`}
+      >
+        Find a better card deal <br className="sm:block hidden" /> in few easy
+        steps.
+      </motion.h2>
+      <motion.p
+        variants={normalText}
+        initial="hidden"
+        whileInView="header"
+        className={`${styles.paragraph} max-w-[470px] mt-5`}
+      >
         Search for a better credit card deal in seconds with our easy-to-use
         comparison tool.
-      </p>
-      <Button styles='mt-10' />
+      </motion.p>
+      <motion.div
+        initial="hidden"
+        whileInView={{ opacity: [0,.5,1], y: [0,15,0],  transition:{
+          duration: 1,
+          times:[0,.5,1],
+          delay: 1,
+          
+        }} }
+        whileHover="hov" variants={button}
+        whileTap="click">
+        <Button styles="mt-10" />
+      </motion.div>
     </div>
     <div className={`${layout.sectionImg}`}>
-      <img src={card} alt="card" className={`w-[100%] h-[100%]`}/>
+      <motion.img
+        initial={{ opacity: 0 }}
+        whileInView={{
+          opacity: 1,
+
+          transition: {
+            duration: 1.5,
+
+            delay: 0.2,
+          },
+        }}
+        src={card}
+        alt="card"
+        className={`w-[100%] h-[100%]`}
+      />
     </div>
   </section>
 );
